bf0c0d99470036225c7de8eb9b19b2afcafe0e96
nepsoul <nirumagar901@gmail.com>
2023-01-29 21:53:35 +0545

    chore: implementing React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+const Home = () => (
+  <div>
+    {" "}
+    <h2>TKTL notes app</h2>{" "}
+  </div>
+);
 
-function App() {
+const Notes = () => (
+  <div>
+    {" "}
+    <h2>Notes</h2>{" "}
+  </div>
+);
+
+const Users = () => (
+  <div>
+    {" "}
+    <h2>Users</h2>{" "}
+  </div>
+);
+
+const App = () => {
+  const padding = {
+    padding: 5,
+  };
+
   return (
-    <div >
-       Learn React
+    <Router>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
       </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
