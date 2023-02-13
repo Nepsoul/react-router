cfa66475eac7d5ba0a9eab0c9aefd76b4fc80854
nepsoul <nirumagar901@gmail.com>
2023-02-14 04:36:28 +0545

    refactor: modifying navigation bar for better UI

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ import {
   Paper,
   TextField,
   Alert,
+  Button,
   Toolbar,
-  IconButton,
+  //IconButton,
   AppBar,
 } from "@mui/material";
-import { Button } from "react-bootstrap";
 
 const Home = () => (
   <div>
@@ -152,23 +152,22 @@ const App = () => {
       {message && <Alert severity="success">{message}</Alert>}
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Button color="inherit">
-            <Link to="/">home</Link>
+          <Button color="inherit" component={Link} to="/">
+            home
           </Button>
-          <Button color="inherit">
-            <Link to="/notes">notes</Link>
+          <Button color="inherit" component={Link} to="/notes">
+            notes
           </Button>
-          <Button color="inherit">
-            <Link to="/users">users</Link>
+          <Button color="inherit" component={Link} to="/users">
+            users
           </Button>
-          <Button color="inherit">
-            {user ? <em>{user} logged in</em> : <Link to="/login">login</Link>}
-          </Button>
+          {user ? (
+            <em>{user} logged in</em>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
 
