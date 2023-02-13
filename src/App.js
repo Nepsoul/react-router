00eb745ea3059dd90ef3ac66863248d54934feab
nepsoul <nirumagar901@gmail.com>
2023-02-14 04:13:38 +0545

    chore:initial setup for metarial ui

--- src/App.js
+++ src/App.js
@@ -9,44 +9,25 @@ import {
   useMatch,
 } from "react-router-dom";
 import { useState } from "react";
-import { Table, Form, Button, Alert, Navbar, Nav } from "react-bootstrap";
 
 const Home = () => (
   <div>
     {" "}
     <h2>TKTL notes app</h2>{" "}
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. It has survived not only five centuries, but
-      also the leap into electronic typesetting, remaining essentially
-      unchanged. It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with desktop
-      publishing software like Aldus PageMaker including versions of Lorem
-      Ipsum.
-    </p>
   </div>
 );
-
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <Table striped>
-      <tbody>
-        {notes.map((note) => (
-          <tr key={note.id}>
-            <td>
-              <Link to={`/notes/${note.id}`}>{note.content}</Link>
-            </td>
-            <td>{note.user}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
-
 const Users = () => (
   <div>
     {" "}
@@ -65,10 +46,8 @@ const Note = ({ note }) => {
     </div>
   );
 };
-
 const Login = (props) => {
   const navigate = useNavigate();
-
   const onSubmit = (event) => {
     event.preventDefault();
     props.onLogin("mluukkai");
@@ -77,20 +56,19 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <Form onSubmit={onSubmit}>
-        <Form.Group>
-          <Form.Label>username:</Form.Label>
-          <Form.Control type="text" name="username" />
-          <Form.Label>password:</Form.Label>
-          <Form.Control type="password" />
-          <Button variant="primary" type="submit">
-            login
-          </Button>
-        </Form.Group>
-      </Form>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input type="username" />
+        </div>
+        <div>
+          password: <input type="password" />
+        </div>
+        <button type="submit">login</button>
+      </form>
     </div>
   );
 };
+
 const App = () => {
   const match = useMatch("/notes/:id");
 
@@ -120,55 +98,33 @@ const App = () => {
     : null;
 
   const [user, setUser] = useState(null);
-  const [message, setMessage] = useState(null);
-
   const login = (user) => {
     setUser(user);
-    setMessage(`Welcome ${user}`);
-    setTimeout(() => {
-      setMessage(null);
-    }, 10000);
   };
-
   const padding = {
     padding: 5,
   };
 
   return (
-    <div className="container">
-      {message && <Alert variant="success">{message}</Alert>}
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/">
-                home
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/notes">
-                notes
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/users">
-                users
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              {user ? (
-                <em style={padding}>{user} logged in</em>
-              ) : (
-                <Link style={padding} to="/login">
-                  login
-                </Link>
-              )}
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+    <div>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to="/login">
+            login
+          </Link>
+        )}
+      </div>
       <Routes>
         <Route path="notes/:id" element={<Note note={note} />} />
         <Route path="/notes" element={<Notes notes={notes} />} />
